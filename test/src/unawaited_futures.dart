553413b966f242235698bd4d316a81d1ccf688a7
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 11:27:30 +1000

    Added copyright notices.

--- test/src/unawaited_futures.dart
+++ test/src/unawaited_futures.dart
@@ -1,3 +1,9 @@
+/* Copyright (C) Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
 Future<String> getStringAsync() async {
   // unawaited_futures should trigger a lint warning.
   final name = getNameAsync();
